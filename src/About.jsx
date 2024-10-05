d0d3058c3b873d0f31f5005ae9c9eef59fdc48f8
Aditya Kumar <adityastark1111@gmail.com>
2024-10-05 15:06:07 +0530

    new changes

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,7 @@ function About() {
                 <div className="About-content">
                     <div className="row">
                         <div className="about-col-1">
-                            <img src="./src/assets/background3.JPG" />
+                            <img src="./../src/assets/background3.JPG" alt='aboutPage Image' />
                         </div>
                         <div className="about-col-2">
                             <h1 className="sub-title">About Me</h1>
